2593869d656f586a89e13d0da528c07f133f7230
jonathas <jonathas09@gmail.com>
2024-08-14 03:28:44 -0300

    teste

--- src/test/java/com/example/java_travel_api/JavaTravelApiApplicationTests.java
+++ src/test/java/com/example/java_travel_api/JavaTravelApiApplicationTests.java
@@ -1,16 +1,2 @@
 package com.example.java_travel_api;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class JavaTravelApiApplicationTests {
-
-
-
-	@Test
-	void contextLoads() {
-		assert true;
-	}
-
-}
