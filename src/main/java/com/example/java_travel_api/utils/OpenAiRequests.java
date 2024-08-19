492cc3ea56b8027df459e2d522a85c98db95e6a1
jonathas <jonathas09@gmail.com>
2024-08-18 22:41:46 -0300

    conf openai

--- src/main/java/com/example/java_travel_api/utils/OpenAiRequests.java
+++ src/main/java/com/example/java_travel_api/utils/OpenAiRequests.java
@@ -3,17 +3,15 @@ package com.example.java_travel_api.utils;
 import com.example.java_travel_api.config.OpenAiConfig;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -24,47 +22,23 @@ public class OpenAiRequests {
 
     private final OpenAiConfig openai;
 
+    @Value("${OPEN_AI_AUTHORIZATION}")
+    private String OPEN_AI_AUTHORIZATION;
+
     public OpenAiRequests(OpenAiConfig openai) {
         this.openai = openai;
     }
 
     public void sendRequestOpenAi(String text) throws JsonProcessingException {
+        try (CloseableHttpClient httpClient = openai.httpClient()) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            HttpPost httpPost = configMessageRequest(text, objectMapper);
 
-        CloseableHttpClient httpClient = openai.httpClient();
-        HttpPost httpPost = openai.createHttpPost();
-
-        // Configuração do corpo da requisição
-        Map<String, Object> requestBody = new HashMap<>();
-        requestBody.put("model", "gpt-4o-mini");
-
-        // Cria uma lista de mensagens
-        List<Map<String, String>> messages = new ArrayList<>();
-        Map<String, String> message = new HashMap<>();
-        message.put("role", "user");
-        message.put("content", "ola, bom dia, apenas um teste");
-        messages.add(message);
-
-        requestBody.put("messages", messages);
-
-        // Converte o corpo da requisição para JSON
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonBody = objectMapper.writeValueAsString(requestBody);
-
-        // Configura o corpo da requisição no HttpPost
-        httpPost.setEntity(new StringEntity(jsonBody, "UTF-8"));
-        httpPost.setHeader("Content-Type", "application/json");
-        httpPost.setHeader("Authorization", "Bearer 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");  // Substitua YOUR_API_KEY pela sua chave
-
-
-
-        // Executa a requisição e processa a resposta
-        try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
+            CloseableHttpResponse response = httpClient.execute(httpPost);
             int statusCode = response.getStatusLine().getStatusCode();
             String responseBody = EntityUtils.toString(response.getEntity(), "UTF-8");
             System.out.println("response body" + responseBody);
             if (statusCode == 200) {
-                System.out.println("Response: " + responseBody);
-
                 Map<String, Object> responseMap = objectMapper.readValue(responseBody, Map.class);
                 List<Map<String, Object>> choices = (List<Map<String, Object>>) responseMap.get("choices");
                 if (choices != null && !choices.isEmpty()) {
@@ -77,5 +51,28 @@ public class OpenAiRequests {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private HttpPost configMessageRequest(String content, ObjectMapper objectMapper) throws JsonProcessingException {
+        HttpPost httpPost = openai.createHttpPost();
+
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("model", "gpt-4o-mini");
+
+        List<Map<String, String>> messages = new ArrayList<>();
+        Map<String, String> message = new HashMap<>();
+        message.put("role", "user");
+        message.put("content", "ola, bom dia, apenas um teste");
+        messages.add(message);
+
+        requestBody.put("messages", messages);
+
+
+        String jsonBody = objectMapper.writeValueAsString(requestBody);
+
+        httpPost.setEntity(new StringEntity(jsonBody, "UTF-8"));
+        httpPost.setHeader("Content-Type", "application/json");
+        httpPost.setHeader("Authorization", OPEN_AI_AUTHORIZATION);
+        return httpPost;
     }
 }
